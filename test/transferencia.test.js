a204ebd4af5549cdb5f299056b9b93cf100f4c64
Natália Gomes Carnelós <nataliagomescarnelos@192.168.0.6>
2026-06-17 14:20:05 -0300

    criei um helper de autenticacao para obter token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,19 +1,13 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require('dotenv').config();
+const { obterToken } = require('../helpers/autenticacao');
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar 201 quando a transferência for criada com sucesso', async () => {
             
-            const respostaLogin = await request(process.env.BASE_URL)
-                            .post('/login')
-                            .set('Content-Type', 'application/json')
-                            .send({
-                                'username': 'julio.lima',
-                                'senha': '123456'
-                            })
-            expect(respostaLogin.status).to.equal(200);
-            const token = respostaLogin.body.token;
+            const token = await obterToken('julio.lima', '123456');
 
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
@@ -31,15 +25,7 @@ describe('Transferencias', () => {
 
         it('Deve retornar 422 quando valor da transferência menor que 10', async () => {
             
-            const respostaLogin = await request(process.env.BASE_URL)
-                            .post('/login')
-                            .set('Content-Type', 'application/json')
-                            .send({
-                                'username': 'julio.lima',
-                                'senha': '123456'
-                            })
-            expect(respostaLogin.status).to.equal(200);
-            const token = respostaLogin.body.token;
+            const token = await obterToken('julio.lima', '123456');
 
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
